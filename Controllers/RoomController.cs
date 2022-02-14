f3943c48f4897da12bde1a3e13f1ffe87d337bd8
cosmin.george8 <459623@student.saxion.nl>
2022-02-14 10:22:33 +0100

    added model classes and created first API endpoint

---
+++ Controllers/RoomController.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("[controller]")]
+public class RoomController : ControllerBase
+{
+    public RoomController()
+    {
+
+    }
+
+    [HttpGet]
+    public ActionResult<List<Room>> GetAll() => RoomReservationService.GetAll();
+
+    [HttpGet("id")]
+    public ActionResult<Room> Get(int Id)
+    {
+        var Room = RoomReservationService.Get(Id);
+        if(Room == null)
+            return NotFound();
+
+        return Room;
+     }
+}
