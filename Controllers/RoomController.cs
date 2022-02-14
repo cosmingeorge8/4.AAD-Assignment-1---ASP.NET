6bf16e40e7a86385bcd006334b565656e754dbfe
George Cosmin Mucalau <459623@student.saxion.nl>
2022-02-14 16:35:30 +0100

    - added Create and Delete endpoints

--- Controllers/RoomController.cs
+++ Controllers/RoomController.cs
@@ -17,9 +17,16 @@ public class RoomController : ControllerBase
 
     }
 
+    /**
+     * Get a list of all rooms
+     */
     [HttpGet]
     public ActionResult<List<Room>> GetAll() => RoomReservationService.GetAll();
 
+    /**
+     * Get Room with specified ID
+     * returns 404 if no room was found
+     */
     [HttpGet("id")]
     public ActionResult<Room> Get(int id)
     {
@@ -31,4 +38,31 @@ public class RoomController : ControllerBase
 
         return room;
     }
+
+    /**
+     * Delete a room with specified Id
+     * Returns 404 if no rooms was found
+     * Returns the list of remaining rooms
+     */
+    [HttpDelete("id")]
+    public ActionResult<List<Room>> Delete(int id)
+    {
+        if (!RoomReservationService.Delete(id))
+        {
+            return NotFound();
+        }
+        return RoomReservationService.GetAll();
+    }
+
+    [HttpPost]
+    public IActionResult Create(Room room)
+    {
+        if (room.Description is  null)
+        {
+            return BadRequest("One of the given fields is null");
+        }
+        Room created = RoomReservationService.Add(room);
+        return Create(room);
+    }
+    
 }
--- Services/RoomReservationService.cs
+++ Services/RoomReservationService.cs
@@ -9,20 +9,49 @@ namespace RoomReservations.Services;
 public static class RoomReservationService
 {
     private static List<Room> Rooms{get;}
+    private static int idCounter = 1;
 
     static RoomReservationService() => Rooms = new List<Room>
     {
-        new Room{Id= 1, Floor = 2, Description = "Presentation Room"},
-        new Room{Id= 2, Floor = 3, Description = "Cool Room"}
+        new Room{Id= idCounter++, Floor = 2, Description = "Presentation Room"},
+        new Room{Id= idCounter++, Floor = 3, Description = "Cool Room"}
     };
 
     public static List<Room> GetAll() => Rooms;
 
     public static Room? Get(int id) => Rooms.FirstOrDefault( r => r.Id == id);
 
-    public static void Add(Room room)
+    public static Room Add(Room room)
     {
-        throw new Exception("Add room not implemented");
-    }   
+        room.Id = idCounter++;
+        Rooms.Add(room);
+        return room;
+    }
+
+    public static bool Delete(int id)
+    {
+        var room = Get(id);
+        if (room is null)
+        {
+            return false;
+        }
+
+        return Rooms.Remove(room);
+    }
+    
+
+    public static bool Update(Room room)
+    {
+        var index = Rooms.FindIndex(r => r.Id == room.Id);
+        if (index == -1)
+        {
+            return false;
+        }
+
+        Rooms[index] = room;
+        return true;
+    }
+    
+    
 
 }
