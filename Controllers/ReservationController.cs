ba3ae660714eecd5138feb61658b25baa56eb5bf
cosmin.george8 <459623@student.saxion.nl>
2022-02-15 12:21:05 +0100

    - code indentation

--- Controllers/ReservationController.cs
+++ Controllers/ReservationController.cs
@@ -12,47 +12,47 @@ namespace RoomReservations.Controllers;
 [Route($"[controller]")]
 public class ReservationController : ControllerBase
 {
- public ReservationController()
- {
- }
- 
- /**
+    public ReservationController()
+    {
+    }
+   
+    /**
      * Get a list of all reservations 
      */
- [HttpGet]
- public ActionResult<List<Reservation>> GetAll() => ReservationService.GetAll();
+    [HttpGet]
+    public ActionResult<List<Reservation>> GetAll() => ReservationService.GetAll();
  
- /**
+    /**
      * Delete a room with specified Id
      * Returns 404 if no rooms was found
      * Returns the list of remaining rooms
      */
- [HttpDelete("id")]
- public ActionResult<List<Reservation>> Delete(int id)
- {
-  if (!ReservationService.Delete(id))
-  {
-   return NotFound();
-  }
-  return ReservationService.GetAll();
- }
+    [HttpDelete("id")]
+    public ActionResult<List<Reservation>> Delete(int id)
+    {
+        if (!ReservationService.Delete(id))
+        {
+            return NotFound();
+        }
+        return ReservationService.GetAll();
+    }
 
- [HttpPost]
- public IActionResult Create(Room room, string date)
- {
-  var user = new User();
-  Reservation created;
+    [HttpPost]
+    public IActionResult Create(Room room, string date)
+    {
+        var user = new User();
+        Reservation created;
 
-  try
-  {
-   created = ReservationService.Add(room, user, date);
-  }
-  catch (Exception e)
-  {
-   return BadRequest(e);
-  }
-  return CreatedAtAction(nameof(Create), new {id = created.Id}, created);
- }
+        try
+        {
+            created = ReservationService.Add(room, user, date);
+        }
+        catch (Exception e)
+        {
+            return BadRequest(e.Message);
+        }
+        return CreatedAtAction(nameof(Create), new {id = created.Id}, created);
+    }
  
  
 }
