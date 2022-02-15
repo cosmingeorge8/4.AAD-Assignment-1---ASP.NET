be0ea36f76979cf6db09326d64e138947dd13743
cosmin.george8 <459623@student.saxion.nl>
2022-02-15 11:47:08 +0100

    - added date utils

---
+++ Models/DateUtil.cs
@@ -0,0 +1,19 @@
+﻿namespace RoomReservations.Models;
+
+public static class DateUtil
+{
+    public static DateOnly GetToday()
+    {
+        return DateOnly.FromDateTime(DateTime.Today);
+    }
+
+    public static DateOnly GetToDate(TimeSpan timeSpan)
+    {
+        return DateOnly.FromDateTime(DateTime.Now.Add(timeSpan));
+    }
+
+    public static bool IsBeforeToday(string date)
+    {
+        return date.CompareTo(GetToday()) < 0;
+    }
+}
