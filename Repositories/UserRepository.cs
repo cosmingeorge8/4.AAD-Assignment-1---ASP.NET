d6b52671ed8a8846ec873abb15623dfbefa59214
cosmin.george8 <459623@student.saxion.nl>
2022-02-16 11:16:37 +0100

    - added user repository and user service classes

---
+++ Repositories/UserRepository.cs
@@ -0,0 +1,15 @@
+﻿using RoomReservations.Models;
+
+namespace RoomReservations.Repositories;
+
+public static class UserRepository
+{
+    public static List<User> Users = new()
+    {
+        new User
+        {
+            Username = "g.mucalau", EmailAddress = "cosminmcl@gmail.com", Password = "gica123", Name = "Cosmin Mucalau"
+        },
+        new User {Username = "a.sas", EmailAddress = "andreisas06@gmail.com", Password = "andrei123", Name = "Andrei Sas"}
+    };
+}
---
+++ Services/UserService.cs
@@ -0,0 +1,15 @@
+﻿using RoomReservations.Models;
+using RoomReservations.Repositories;
+
+namespace RoomReservations.Services;
+
+
+public class UserService
+{
+    public static User? Get(UserLogin userLogin)
+    {
+        return UserRepository.Users.FirstOrDefault(user => 
+            user.Username.Equals(userLogin.Username, StringComparison.Ordinal) && 
+            user.Password.Equals(userLogin.Password));
+    }
+}
