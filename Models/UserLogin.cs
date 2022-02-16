9102114eb972ce97a4fa53149934ac7c03daf4c6
cosmin.george8 <459623@student.saxion.nl>
2022-02-16 11:16:21 +0100

    - added userLogin class

---
+++ Models/UserLogin.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.IdentityModel.Tokens;
+
+namespace RoomReservations.Models;
+
+/**
+ * @author Mucalau Cosmin
+ *
+ * The class's purpose is to encapsulate the user authentication attempts
+ */
+public class UserLogin
+{
+    public string Username { get; set; }
+
+    public string Password { get; set; }
+
+    public bool IsValid()
+    {
+       return !Username.IsNullOrEmpty() && !Password.IsNullOrEmpty();
+    }
+}
