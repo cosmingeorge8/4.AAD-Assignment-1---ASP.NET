c4b78f1c27b54cd087f852f44e12ce9aac263015
cosmin.george8 <459623@student.saxion.nl>
2022-02-16 11:16:07 +0100

    - added field to user class

--- Models/User.cs
+++ Models/User.cs
@@ -6,4 +6,11 @@ namespace RoomReservations.Models;
 
 public class User
 {
+    public string Username { get; set; }
+    
+    public string EmailAddress { get; set; }
+    
+    public string Password { get; set; }
+
+    public string Name { get; set; }
 }
