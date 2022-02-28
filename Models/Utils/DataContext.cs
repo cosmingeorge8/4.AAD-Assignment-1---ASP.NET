d050ba56786cece4259857a3ee77baf6ffe1c13e
cosmin.george8 <459623@student.saxion.nl>
2022-02-28 08:48:46 +0100

    - added DataContext.cs

---
+++ Models/Utils/DataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RoomReservations.Models.Utils;
+
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+    public DbSet<Reservation> Reservations { get; set; }
+
+    public DbSet<User> Users { get; set; }
+    
+    public DbSet<Room> Rooms { get; set; }
+}
---
+++ Repositories/RoomRepository.cs
@@ -0,0 +1,16 @@
+﻿
+using RoomReservations.Models;
+using RoomReservations.Models.Utils;
+
+namespace RoomReservations.Repositories;
+
+public class RoomRepository
+{
+    private DataContext _dataContext;
+
+    public async Task<Room?> Get(int roomId)
+    {
+        return await _dataContext.Rooms.FindAsync(roomId);
+    }
+
+}
